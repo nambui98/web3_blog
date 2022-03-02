473cbbac1597c979562b8add0cece8684be08542
nambui98 <bvnam98@gmail.com>
2022-03-02 21:57:39 +0700

    update

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
 export const contractAddress = "0xA89EafC886C17f175A0fCC1FDee1aE8636f1d313"
-export const ownerAddress = "0x3EbC1D99E2B1F89ECCc18cFc5B0eA5fa74C5c0a3"
+export const ownerAddress = "0x3F435be7f52e83e9A5784953220B46bC31D1AAF7"
